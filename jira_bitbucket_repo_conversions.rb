186b805167333bf5167bb6a8d36a1b890e1c53fd
Kiffin Gish <kiffin.gish@planet.nl>
2021-08-13 12:59:19 +0200

    Added helper jira_bitbucket_repo_conversions helper script.

--- jira_bitbucket_repo_conversions.rb
+++ jira_bitbucket_repo_conversions.rb
@@ -21,6 +21,13 @@ column_names = %w{
   assembla_repo_url
 }
 
+@assembla_space_keys = []
+@assembla_space_names = []
+@assembla_repo_names = []
+@assembla_repo_urls = []
+@bitbucket_repo_names = []
+@bitbucket_repo_urls = []
+
 # assembla_space_ey,assembla_space_name,assembla_repo_name,bitbucket_repo_name,bitbucket_repo_url,assembla_repo_url
 repos = csv_to_array(BITBUCKET_REPO_CONVERSIONS)
 
@@ -51,7 +58,7 @@ column_names.each do |column_name|
   puts "* #{column_name}"
 end
 
-conversions = {}
+@conversions = {}
 repos.each do |repo|
   assembla_space_key = repo['assembla_space_key']
   assembla_space_name = repo['assembla_space_name']
@@ -60,9 +67,16 @@ repos.each do |repo|
   bitbucket_repo_name = repo['bitbucket_repo_name']
   bitbucket_repo_url = repo['bitbucket_repo_url']
 
+  @assembla_space_keys << assembla_space_key unless @assembla_space_keys.include?(assembla_space_key)
+  @assembla_space_names << assembla_space_name unless @assembla_space_names.include?(assembla_space_name)
+  @assembla_repo_names << assembla_repo_name unless @assembla_repo_names.include?(assembla_repo_name)
+  @assembla_repo_urls << assembla_repo_url unless @assembla_repo_urls.include?(assembla_repo_url)
+  @bitbucket_repo_names << bitbucket_repo_name unless @bitbucket_repo_names.include?(bitbucket_repo_name)
+  @bitbucket_repo_urls << bitbucket_repo_url unless @bitbucket_repo_urls.include?(bitbucket_repo_url)
+
   conversions_key = "#{assembla_space_key}#{DELIMITER}#{assembla_space_name}"
-  conversions[conversions_key] ||= []
-  conversions[conversions_key] << {
+  @conversions[conversions_key] ||= []
+  @conversions[conversions_key] << {
     assembla_repo_name: assembla_repo_name,
     assembla_repo_url: assembla_repo_url,
     bitbucket_repo_name: bitbucket_repo_name,
@@ -70,7 +84,25 @@ repos.each do |repo|
   }
 end
 
-conversions.each do |key, values|
+puts "\nTotal assembla space keys: #{@assembla_space_keys.count}"
+@assembla_space_keys.each { |name| puts "* #{name}"}
+
+puts "\nTotal assembla space names: #{@assembla_space_names.count}"
+@assembla_space_names.each { |name| puts "* #{name}"}
+
+puts "\nTotal assembla repo names: #{@assembla_repo_names.count}"
+@assembla_repo_names.each { |name| puts "* #{name}"}
+
+puts "\nTotal assembla repo urls: #{@assembla_repo_urls.count}"
+@assembla_repo_urls.each { |name| puts "* #{name}"}
+
+puts "\nTotal bitbucket repo names: #{@bitbucket_repo_names.count}"
+@bitbucket_repo_names.each { |name| puts "* #{name}"}
+
+puts "\nTotal bitbucket repo_urls: #{@bitbucket_repo_urls.count}"
+@bitbucket_repo_urls.each { |name| puts "* #{name}"}
+
+@conversions.each do |key, values|
   (assembla_space_key, assembla_space_name) = key.split(DELIMITER)
   puts "#{assembla_space_key} | #{assembla_space_name}"
   values.each do |value|
@@ -82,3 +114,21 @@ conversions.each do |key, values|
     puts "  bitbucket repo_name='#{bitbucket_repo_name}' repo_url='#{bitbucket_repo_url}'"
   end
 end
+
+def get_conversion(assembla_space_key)
+  @conversions.detect { |key| key.split(DELIMITER)[0] == assembla_space_key }
+end
+
+def assembla_to_bitbucket_repo_url(assembla_space_key, assembla_repo_url)
+  bitbucket_repo_url = nil
+  found_conversion = get_conversion(assembla_space_key)
+  if found_conversion
+    found_value = found_conversion.values.detect { |value| value[:assembla_repo_url] == assembla_repo_url}
+    if found_value
+      bitbucket_repo_url = found_value[:bitbucket_repo_url]
+    end
+  end
+  bitbucket_repo_url
+end
+
+# Sanity checks.
